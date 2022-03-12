355bf2c4ed524c71cc150a6ed55a0393c65d4635
Rudy Rodarte <shadyrudy@hotmail.com>
2022-03-12 17:24:23 -0600

    Finished Chapter 7
    Each blog post gets it's own folder.
    added MDX file
    This is how we handle images now.

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,16 +1,22 @@
 import * as React from 'react'
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import Layout from '../../components/layout'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../../components/layout';
 
 const BlogPost = ({data}) => {
     console.log();
+    const image = getImage(data.mdx.frontmatter.hero_image);
 
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>
                 {data.mdx.frontmatter.date}
             </p>
+            <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+            />
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -19,15 +25,23 @@ const BlogPost = ({data}) => {
 }
 
 export const query = graphql`
-query ($id: String) {
+  query($id: String) {
     mdx(id: {eq: $id}) {
-        frontmatter {
-            title
-            date(formatString: "YYYY-MM-DD")
+      body
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
         }
-        body
+      }
     }
-}
+  }
 `
 
 export default BlogPost
